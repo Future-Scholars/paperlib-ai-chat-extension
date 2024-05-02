8ecbc1ef10b8c0fac016d09dc7cddc47ee3deb1c
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-05-02 02:12:37 -0700

    feat: remove unused files in dist

--- scripts/dep.cjs
+++ scripts/dep.cjs
@@ -19,6 +19,14 @@ fs.writeFileSync(path.resolve(__dirname, "..", "dist", "package.json"), JSON.str
 execSync("npm install --production", {cwd: path.resolve(__dirname, "..", "dist")})
 
 
-// fs.rmSync(path.resolve(__dirname, "..", "dist", "node_modules", "onnxruntime-web"), {force: true, recursive: true})
+const NODE_MODULES_PATH= path.resolve(__dirname, "..", "dist", "node_modules")
+
+const filePaths = fs.readdirSync(NODE_MODULES_PATH,{recursive:true})
+
+for (const filePath of filePaths){
+    if ([".wasm",".map",".ts",".md"].some((fileType)=>filePath.endsWith(fileType))){
+        fs.rmSync(path.resolve(NODE_MODULES_PATH,filePath), {force: true,recursive:true})
+    }
+}
 
 
