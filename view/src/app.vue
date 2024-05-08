6c0c30ba60616f21b8c0c1d3e5eaa96c76a6b634
geoffreychen777 <geoffreychen777@gmail.com>
2024-05-08 18:05:02 +0100

    fix: use spinner

--- view/src/app.vue
+++ view/src/app.vue
@@ -8,7 +8,6 @@ import {
   BIconX,
   BIconBoxArrowUpRight,
   BIconBoxArrowInDownLeft,
-  BIconPauseCircle,
 } from "bootstrap-icons-vue";
 
 const windowID = "paperlib-ai-chat-extension-window";
@@ -80,7 +79,7 @@ const loadPaperText = async () => {
   });
 
   const selectedPaperEntities = (await PLAPI.uiStateService.getState(
-    "selectedPaperEntities",
+    "selectedPaperEntities"
   )) as PaperEntity[];
 
   const paperEntity =
@@ -104,7 +103,7 @@ const loadPaperText = async () => {
 
 const closeWindow = () => {
   PLMainAPI.windowProcessManagementService.forceClose(
-    "paperlib-ai-chat-extension-window",
+    "paperlib-ai-chat-extension-window"
   );
   if (pinned.value) {
     PLMainAPI.windowProcessManagementService.focus(processId.renderer);
@@ -137,7 +136,7 @@ const sendMessage = async (event: KeyboardEvent) => {
 
     const answer = await chatService.queryLLM(msg, context);
     const targetIndex = messageList.value.findIndex(
-      (item) => item.id === receivedMsgId,
+      (item) => item.id === receivedMsgId
     );
     if (targetIndex !== -1) {
       messageList.value[targetIndex] = {
@@ -157,12 +156,12 @@ const unpin = async () => {
   await nextTick(async () => {
     await PLMainAPI.windowProcessManagementService.setParentWindow(
       null,
-      windowID,
+      windowID
     );
     await PLMainAPI.windowProcessManagementService.center(windowID);
     await PLMainAPI.windowProcessManagementService.setAlwaysOnTop(
       windowID,
-      true,
+      true
     );
   });
 };
@@ -171,19 +170,19 @@ const pin = async () => {
   pinned.value = true;
   await PLMainAPI.windowProcessManagementService.setParentWindow(
     processId.renderer,
-    windowID,
+    windowID
   );
   await PLMainAPI.windowProcessManagementService.fire({
     [windowID]: "pin-window",
   });
   await PLMainAPI.windowProcessManagementService.setAlwaysOnTop(
     windowID,
-    false,
+    false
   );
 };
 
 disposable(
-  PLAPI.uiStateService.onChanged(["selectedPaperEntities"], loadPaperText),
+  PLAPI.uiStateService.onChanged(["selectedPaperEntities"], loadPaperText)
 );
 
 onMounted(() => {
@@ -268,7 +267,24 @@ onMounted(() => {
         @focus="handleMsgInputFocus"
         @blur="handleMsgInputBlur"
       />
-      <BIconPauseCircle class="pr-1 text-2xl" v-if="loading || !ready" />
+      <div class="w-7">
+        <svg
+          role="status"
+          class="animate-spin w-3.5 h-3.5 text-neutral-400 fill-neutral-700"
+          viewBox="0 0 100 101"
+          fill="none"
+          v-if="loading || !ready"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+      </div>
     </div>
   </div>
 </template>
