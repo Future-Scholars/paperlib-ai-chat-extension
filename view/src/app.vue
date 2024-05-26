873555e871f79856a8a68e9248249d5e0ab5485e
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-05-27 05:22:47 +0800

    feat: MessageInput

--- view/src/app.vue
+++ view/src/app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { nextTick, onMounted, ref } from "vue";
+import { nextTick, onMounted, Ref, ref } from "vue";
 import { PLAPI, PLMainAPI } from "paperlib-api/api";
 import { PaperEntity } from "paperlib-api/model";
 import { disposable } from "@/base/dispose.ts";
@@ -9,6 +9,7 @@ import {
   BIconBoxArrowUpRight,
   BIconBoxArrowInDownLeft,
 } from "bootstrap-icons-vue";
+import { MessageInput } from "./components";
 
 const windowID = "paperlib-ai-chat-extension-window";
 
@@ -34,7 +35,8 @@ const curPaperEntity = ref<
 
 const messageList = ref([...INIT_MESSAGE_LIST]);
 
-const msgInputRef = ref<HTMLInputElement | null>(null);
+const msgInputRef = ref<{ inputRef: HTMLInputElement | null } | null>(null);
+
 const msgListRef = ref<HTMLDivElement | null>(null);
 
 const pinned = ref(true);
@@ -56,14 +58,14 @@ const handleKeyDown = (event: KeyboardEvent) => {
 };
 
 const handleMsgInputFocus = () => {
-  if (msgInputRef.value) {
-    msgInputRef.value.addEventListener("keydown", handleKeyDown);
+  if (msgInputRef.value?.inputRef) {
+    msgInputRef.value.inputRef.addEventListener("keydown", handleKeyDown);
   }
 };
 
 const handleMsgInputBlur = () => {
-  if (msgInputRef.value) {
-    msgInputRef.value.removeEventListener("keydown", handleKeyDown);
+  if (msgInputRef.value?.inputRef) {
+    msgInputRef.value.inputRef.removeEventListener("keydown", handleKeyDown);
   }
 };
 
@@ -79,7 +81,7 @@ const loadPaperText = async () => {
   });
 
   const selectedPaperEntities = (await PLAPI.uiStateService.getState(
-    "selectedPaperEntities"
+    "selectedPaperEntities",
   )) as PaperEntity[];
 
   const paperEntity =
@@ -103,7 +105,7 @@ const loadPaperText = async () => {
 
 const closeWindow = () => {
   PLMainAPI.windowProcessManagementService.forceClose(
-    "paperlib-ai-chat-extension-window"
+    "paperlib-ai-chat-extension-window",
   );
   if (pinned.value) {
     PLMainAPI.windowProcessManagementService.focus(processId.renderer);
@@ -136,7 +138,7 @@ const sendMessage = async (event: KeyboardEvent) => {
 
     const answer = await chatService.queryLLM(msg, context);
     const targetIndex = messageList.value.findIndex(
-      (item) => item.id === receivedMsgId
+      (item) => item.id === receivedMsgId,
     );
     if (targetIndex !== -1) {
       messageList.value[targetIndex] = {
@@ -158,12 +160,12 @@ const unpin = async () => {
   await nextTick(async () => {
     await PLMainAPI.windowProcessManagementService.setParentWindow(
       null,
-      windowID
+      windowID,
     );
     await PLMainAPI.windowProcessManagementService.center(windowID);
     await PLMainAPI.windowProcessManagementService.setAlwaysOnTop(
       windowID,
-      true
+      true,
     );
   });
 };
@@ -172,19 +174,19 @@ const pin = async () => {
   pinned.value = true;
   await PLMainAPI.windowProcessManagementService.setParentWindow(
     processId.renderer,
-    windowID
+    windowID,
   );
   await PLMainAPI.windowProcessManagementService.fire({
     [windowID]: "pin-window",
   });
   await PLMainAPI.windowProcessManagementService.setAlwaysOnTop(
     windowID,
-    false
+    false,
   );
 };
 
 disposable(
-  PLAPI.uiStateService.onChanged(["selectedPaperEntities"], loadPaperText)
+  PLAPI.uiStateService.onChanged(["selectedPaperEntities"], loadPaperText),
 );
 
 onMounted(() => {
@@ -256,38 +258,12 @@ onMounted(() => {
         </div>
       </div>
     </div>
-    <div
-      id="input-box "
-      class="flex-none flex space-x-2 text-neutral-800 mx-3 my-3 bg-neutral-200 rounded-md flex-row items-center dark:bg-neutral-700 dark:text-neutral-300"
-    >
-      <input
-        type="text"
-        id="msg-input"
-        class="w-full p-2 grow outline-none text-sm bg-transparent"
-        ref="msgInputRef"
-        placeholder="Type your question here..."
-        @focus="handleMsgInputFocus"
-        @blur="handleMsgInputBlur"
-      />
-      <div class="w-7">
-        <svg
-          role="status"
-          class="animate-spin w-3.5 h-3.5 text-neutral-400 fill-neutral-700"
-          viewBox="0 0 100 101"
-          fill="none"
-          v-if="loading || !ready"
-        >
-          <path
-            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-            fill="currentColor"
-          />
-          <path
-            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-            fill="currentFill"
-          />
-        </svg>
-      </div>
-    </div>
+    <message-input
+      :disabled="loading || !ready"
+      @focus="handleMsgInputFocus"
+      @blur="handleMsgInputBlur"
+      ref="msgInputRef"
+    />
   </div>
 </template>
 
---
+++ view/src/components/index.ts
@@ -0,0 +1,3 @@
+import MessageInput from "./MessageInput/index.vue";
+
+export { MessageInput };
