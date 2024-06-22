fcbecaa81be1e3bcce94e59683e0b92a2d0190f4
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-06-23 07:22:53 +0800

    refactor(persist): extract methods

--- view/src/utils/persist.ts
+++ view/src/utils/persist.ts
@@ -1,6 +1,11 @@
 import localForage from "localforage";
-import { MessageState, useMessageStore } from "@/store/message.ts";
 import {
+  MESSAGE_STORE_ID,
+  MessageState,
+  useMessageStore,
+} from "@/store/message.ts";
+import {
+  CONVERSATION_STORE_ID,
   ConversationState,
   useConversationStore,
 } from "@/store/conversation.ts";
@@ -13,62 +18,76 @@ localForage.config({
   name: "ai-chat-extension",
 });
 
+const MESSAGE_PERSIST_ID = MESSAGE_STORE_ID + "-state";
+const CONVERSATION_PERSIST_ID = CONVERSATION_STORE_ID + "-state";
+
 export function usePersistState() {
   const messageStore = useMessageStore();
   const conversationStore = useConversationStore();
   const loading = ref(true);
 
+  async function persistMessage() {
+    const storedMessage =
+      await localForage.getItem<MessageState>(MESSAGE_PERSIST_ID);
+    //Delete fake messages
+    if (storedMessage?.entity) {
+      for (const id in storedMessage.entity) {
+        if (storedMessage.entity[id].fake) {
+          delete storedMessage.entity[id];
+        }
+      }
+
+      messageStore.$patch(storedMessage);
+    }
+
+    messageStore.$subscribe(() => {
+      localForage.setItem(
+        MESSAGE_PERSIST_ID,
+        JSON.parse(JSON.stringify(messageStore.$state)),
+      ); // Destructure to transform to plain object
+    });
+  }
+
+  async function persistConversation() {
+    const storedConversation = await localForage.getItem<ConversationState>(
+      CONVERSATION_PERSIST_ID,
+    );
+
+    if (storedConversation?.entity) {
+      const conversations = Object.values(storedConversation.entity);
+      if (conversations.length > MAX_CONVERSATION_NUM) {
+        conversations.sort((a, b) => {
+          return b.timestamp - a.timestamp;
+        });
+        const newConversationIds = conversations
+          .slice(0, MAX_CONVERSATION_NUM)
+          .map((item) => item.id);
+
+        for (const id in storedConversation.entity) {
+          if (
+            !newConversationIds.includes(
+              id as ReturnType<typeof crypto.randomUUID>,
+            )
+          ) {
+            delete storedConversation.entity[id];
+          }
+        }
+      }
+      conversationStore.$patch(storedConversation);
+    }
+
+    conversationStore.$subscribe(() => {
+      localForage.setItem(
+        CONVERSATION_PERSIST_ID,
+        JSON.parse(JSON.stringify(conversationStore.$state)),
+      ); // Destructure to transform to plain object
+    });
+  }
+
   onMounted(async () => {
     try {
-      const storedMessage = await localForage.getItem<MessageState>(
-        messageStore.$id + "-state",
-      );
-      //Delete fake messages
-      if (storedMessage?.entity) {
-        for (const id in storedMessage.entity) {
-          if (storedMessage.entity[id].fake) {
-            delete storedMessage.entity[id];
-          }
-        }
-
-        messageStore.$patch(storedMessage);
-      }
-
-      const storedConversation = await localForage.getItem<ConversationState>(
-        conversationStore.$id + "-state",
-      );
-
-      if (storedConversation?.entity) {
-        const conversations = Object.values(storedConversation.entity);
-        if (conversations.length > MAX_CONVERSATION_NUM) {
-          conversations.sort((a, b) => {
-            return b.timestamp - a.timestamp;
-          });
-          const newConversationIds = conversations
-            .slice(0, MAX_CONVERSATION_NUM)
-            .map((item) => item.id);
-
-          for (const id in storedConversation.entity) {
-            if (
-              !newConversationIds.includes(
-                id as ReturnType<typeof crypto.randomUUID>,
-              )
-            ) {
-              delete storedConversation.entity[id];
-            }
-          }
-        }
-        conversationStore.$patch(storedConversation);
-      }
-
-      [messageStore, conversationStore].forEach((store) => {
-        store.$subscribe(() => {
-          localForage.setItem(
-            store.$id + "-state",
-            JSON.parse(JSON.stringify(store.$state)),
-          ); // Destructure to transform to plain object
-        });
-      });
+      await persistMessage();
+      await persistConversation();
     } finally {
       loading.value = false;
     }
