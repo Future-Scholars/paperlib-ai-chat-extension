0f7ee39133270fc04fa3aae54a6633f1fd9fec3f
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-08-17 16:56:07 +0800

    refactor(pdfParser): remove unused code

--- view/src/utils/pdfParser/index.ts
+++ view/src/utils/pdfParser/index.ts
@@ -4,7 +4,7 @@ export enum PDF_PARSER_TYPE {
   LLM_PARSER,
   MUPDF,
 }
-export async function newPdfParser(type: PDF_PARSER_TYPE, url: string) {
+export async function newPdfParser(type: PDF_PARSER_TYPE) {
   if (type === PDF_PARSER_TYPE.MUPDF) {
     return new MupdfParser();
   }
