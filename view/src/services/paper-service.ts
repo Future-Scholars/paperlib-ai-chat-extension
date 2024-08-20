1d974e6b674047bde37194100a8899ce9ffea1b0
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-08-20 21:42:32 +0800

    feat(PaperService): encode

--- view/src/services/paper-service.ts
+++ view/src/services/paper-service.ts
@@ -1,9 +1,16 @@
 import { PdfParser } from "@/utils/pdfParser/parser.ts";
 import { PLExtAPI } from "paperlib-api/api";
 import { newPdfParser, PDF_PARSER_TYPE } from "@/utils/pdfParser";
+import { EncoderService } from "@/services/encoder-service.ts";
 
 export default class PaperService {
   private pdfParser?: PdfParser;
+  private readonly encoderService: EncoderService;
+
+  constructor() {
+    this.encoderService = new EncoderService();
+  }
+
   async init() {
     const useLLAMAParse = (await PLExtAPI.extensionPreferenceService.get(
       "@future-scholars/paperlib-ai-chat-extension",
@@ -34,5 +41,27 @@ export default class PaperService {
       onProgress?.(progress);
     }
     return contents;
+  }
+
+  async encode(url: string, onProgress?: (progress: number) => void) {
+    const fulltext = (
+      await this.parsePdf(url, (progress) => {
+        onProgress?.((progress / 100) * 50);
+      })
+    ).join("\n");
+
+    const embeddings: { text: string; embedding: number[] }[] = [];
+    const words = fulltext.split(" ");
+    const paragraphs: string[] = [];
+    for (let i = 0; i < words.length; i += 256) {
+      paragraphs.push(words.slice(i, i + 256).join(" "));
+    }
+
+    for (let p = 0; p < paragraphs.length; p++) {
+      const embedding = await this.encoderService.encode(paragraphs[p]);
+      embeddings.push({ text: paragraphs[p], embedding });
+      onProgress?.(50 + ((p + 1) / paragraphs.length) * 100);
+    }
+    return embeddings;
   }
 }
